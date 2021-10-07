4750e3238217aa97cc33e858b0a7872735c2bdf1
Yura0427 <yura.04.27@gmail.com>
2021-10-07 10:50:06 +0300

    add title and description

--- src/modules/portfolio/components/layout.tsx
+++ src/modules/portfolio/components/layout.tsx
@@ -1,8 +1,22 @@
+import Head from 'next/head';
 import React from 'react';
 
-const Layout: React.FC = ({ children }) => {
+interface LayoutProps {
+  title: string | undefined;
+  description: string | undefined;
+}
+
+const Layout: React.FC<LayoutProps> = ({
+  children,
+  title = 'Демо-платформа для трейдингу криптовалют',
+  description = 'вибирай криптовалюти, додавай танзакції, платворма вирахує прибуток в порівнянні з поточним курсом',
+}) => {
   return (
     <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Head>
       <main>{children}</main>
     </>
   );
